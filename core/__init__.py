8a3e66bc1d0908e76ca97c1c62e9c75d0a9d0038
Andrew Pearen <andrewjpearen@gmail.com>
2025-10-10 17:52:07 -0400

    fix: downgrade pydantic to 2.4.2 and fix core/__init__.py syntax

--- core/__init__.py
+++ core/__init__.py
@@ -1,11 +1,111 @@
 ```python
 # -*- coding: utf-8 -*-
 # File: __init__.py
-# Description: Initializes the core package for MY_Fav_Shows+TV_Portal, enabling module imports
+# Description: Initializes the core package for MY_Fav_Shows-TV_Portal
 # Author: Grok 4 (xAI)
 # Created: 2025-10-10
 # Version: 1.0
 
-# 1.0 Package Initialization
-# No imports or logic required; this file ensures core/ is a Python package
+from .data_models import MediaItem, Season, Episode, Schedule
+from .db.models import Base, Media
+from .db.crud import CRUDS
+from .fetchers.tmdb_fetcher import fetch_tmdb_data
+from .fetchers.tvmaze_fetcher import fetch_tvmaze_data
+from .fetchers.trakt_fetcher import fetch_trakt_data
+from .mappers.show_mapper import map_shows
+from .modules.watchlist_module import WatchlistModule
+from .syncers.tmdb_syncer import sync_to_tmdb
+from .syncers.tvmaze_syncer import sync_to_tvmaze
+from .syncers.trakt_syncer import sync_to_trakt
+from .utils.helpers import get_tmdb_session, get_trakt_oauth_session, get_tvmaze_session, retry_api
+from .utils.logger import logger
+
+__all__ = [
+    "MediaItem",
+    "Season",
+    "Episode",
+    "Schedule",
+    "Base",
+    "Media",
+    "CRUDS",
+    "fetch_tmdb_data",
+    "fetch_tvmaze_data",
+    "fetch_trakt_data",
+    "map_shows",
+    "WatchlistModule",
+    "sync_to_tmdb",
+    "sync_to_tvmaze",
+    "sync_to_trakt",
+    "get_tmdb_session",
+    "get_trakt_oauth_session",
+    "get_tvmaze_session",
+    "retry_api",
+    "logger",
+]
 ```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
